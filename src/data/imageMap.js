ea7f68c9758630c3dd65590bfe8b11946d0d4666
GameGaurd <Mightyband8989@proton.me>
2026-09-01 08:22:22 +0000

    Add real Wix product images for 17 popular items

--- src/data/imageMap.js
+++ src/data/imageMap.js
@@ -4,6 +4,28 @@
 
 export const itemImageMap = {
   'Crystal Fragment': 'https://static.wixstatic.com/media/9f8412_166e4068510c43ffbc2e368738a91fe7~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_166e4068510c43ffbc2e368738a91fe7~mv2.jpg',
+  
+  // Love Badges & Rewards items
+  'OPT': 'https://static.wixstatic.com/media/9f8412_62099dd52f1f467b9bdc02272aa370cb~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_62099dd52f1f467b9bdc02272aa370cb~mv2.jpg',
+  'ACCE': 'https://static.wixstatic.com/media/9f8412_32221e0aa8a549de9b093de4fa76b1ef~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_32221e0aa8a549de9b093de4fa76b1ef~mv2.jpg',
+  'PRI OPT': 'https://static.wixstatic.com/media/9f8412_4a08044e21334f40a7e8628ddc2780e5~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_4a08044e21334f40a7e8628ddc2780e5~mv2.jpg',
+  'HG': 'https://static.wixstatic.com/media/9f8412_c1ebc9971207432a883e06b2be06d398~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_c1ebc9971207432a883e06b2be06d398~mv2.jpg',
+  'MOUNT': 'https://static.wixstatic.com/media/9f8412_eb186e0bd90b4e519120dcd990cb734d~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_eb186e0bd90b4e519120dcd990cb734d~mv2.jpg',
+  'ITEM': 'https://static.wixstatic.com/media/9f8412_07cfef373ec5414b8b099c7f87889705~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_07cfef373ec5414b8b099c7f87889705~mv2.jpg',
+  'TITLE': 'https://static.wixstatic.com/media/9f8412_bdf356fd8fec4c7cb5a119b11e9598fe~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_bdf356fd8fec4c7cb5a119b11e9598fe~mv2.jpg',
+  'WT': 'https://static.wixstatic.com/media/9f8412_c75cc4688c7e448aba5b69c02814e966~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_c75cc4688c7e448aba5b69c02814e966~mv2.jpg',
+  
+  // Individual popular items
+  'Zhi Yuan': 'https://static.wixstatic.com/media/9f8412_4ce4d85e9f9b4fe0b98a0f0d793bcf3a~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_4ce4d85e9f9b4fe0b98a0f0d793bcf3a~mv2.jpg',
+  'Bamboo': 'https://static.wixstatic.com/media/9f8412_51502a8271594c08a54e61e7436b1a67~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_51502a8271594c08a54e61e7436b1a67~mv2.jpg',
+  'Fuyue': 'https://static.wixstatic.com/media/9f8412_8c7dde1409054876a7c47611b5af5d6f~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_8c7dde1409054876a7c47611b5af5d6f~mv2.jpg',
+  'Love Roots': 'https://static.wixstatic.com/media/9f8412_1d13fb913a904361b831b2cdafb2f668~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_1d13fb913a904361b831b2cdafb2f668~mv2.jpg',
+  'Sacred Flame': 'https://static.wixstatic.com/media/9f8412_bd04c9c8b7ba4bb78f840ddd22803d85~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_bd04c9c8b7ba4bb78f840ddd22803d85~mv2.jpg',
+  'Divine Dance': 'https://static.wixstatic.com/media/9f8412_532c8032d0c746e398965e0b531970e1~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_532c8032d0c746e398965e0b531970e1~mv2.jpg',
+  'Focus': 'https://static.wixstatic.com/media/9f8412_9c22e2ceb31f465aa631246e158eac3c~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_9c22e2ceb31f465aa631246e158eac3c~mv2.jpg',
+  'Blooming Vow': 'https://static.wixstatic.com/media/9f8412_c89d99ad131c40159964c239393f83a9~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_c89d99ad131c40159964c239393f83a9~mv2.jpg',
+  'Lyrical': 'https://static.wixstatic.com/media/9f8412_ed4bdb9bf75649cd8d037fc59078f285~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_ed4bdb9bf75649cd8d037fc59078f285~mv2.jpg',
+  'Advance': 'https://static.wixstatic.com/media/9f8412_20e16b8df6a84f18a6e51110f25d3237~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_20e16b8df6a84f18a6e51110f25d3237~mv2.jpg',
 };
 
 export function getItemImage(itemName) {
