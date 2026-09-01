8b48a1da286fe588e4b2ba7a30f08d105b71ef26
GameGaurd <Mightyband8989@proton.me>
2026-09-01 08:51:34 +0000

    Add 13 real game item images from RC Egg blog posts

--- src/data/imageMap.js
+++ src/data/imageMap.js
@@ -31,6 +31,21 @@ export const itemImageMap = {
   'Blooming Vow': 'https://static.wixstatic.com/media/9f8412_c89d99ad131c40159964c239393f83a9~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_c89d99ad131c40159964c239393f83a9~mv2.jpg',
   'Lyrical': 'https://static.wixstatic.com/media/9f8412_ed4bdb9bf75649cd8d037fc59078f285~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_ed4bdb9bf75649cd8d037fc59078f285~mv2.jpg',
   'Advance': 'https://static.wixstatic.com/media/9f8412_20e16b8df6a84f18a6e51110f25d3237~mv2.jpg/v1/fill/w_1000,h_714,al_c,q_85/9f8412_20e16b8df6a84f18a6e51110f25d3237~mv2.jpg',
+  
+  // RC Egg items - from various event blog posts
+  'Proche BK/WT': 'https://static.wixstatic.com/media/9f8412_b38fea0c80b54e1f97dd3d25bdcf02e3~mv2.jpg/v1/fill/w_812,h_85,al_c,lg_1,q_80/9f8412_b38fea0c80b54e1f97dd3d25bdcf02e3~mv2.jpg',
+  'Hummer GN': 'https://static.wixstatic.com/media/9f8412_6ab169102aa2407d9814af057f28e5c3~mv2.png/v1/fill/w_1000,h_482,al_c,q_90,usm_0.66_1.00_0.01/9f8412_6ab169102aa2407d9814af057f28e5c3~mv2.png',
+  'Summer Happy Chair BK/WT': 'https://static.wixstatic.com/media/9f8412_10e74a058e3746fd84093e546715b83a~mv2.png/v1/fill/w_1000,h_785,al_c,q_90,usm_0.66_1.00_0.01/9f8412_10e74a058e3746fd84093e546715b83a~mv2.png',
+  'Rocking Chair': 'https://static.wixstatic.com/media/9f8412_86ec785261a94e82b115073b0743f709~mv2.jpg/v1/fill/w_1000,h_632,al_c,q_85,usm_0.66_1.00_0.01/9f8412_86ec785261a94e82b115073b0743f709~mv2.jpg',
+  'Demon Guitar BK/WT': 'https://static.wixstatic.com/media/9f8412_c407d0beacc547018abc2de877761e08~mv2.jpg/v1/fill/w_883,h_495,al_c,q_85/9f8412_c407d0beacc547018abc2de877761e08~mv2.jpg',
+  'Dream Umbrella BK': 'https://static.wixstatic.com/media/9f8412_59305385b1864908bdb95c51d8a10d74~mv2.png/v1/fill/w_1000,h_991,al_c,q_90,usm_0.66_1.00_0.01/9f8412_59305385b1864908bdb95c51d8a10d74~mv2.png',
+  'Kitty Swing OPT': 'https://static.wixstatic.com/media/9f8412_61c81d2acab442dc877e2449ee89ae07~mv2.jpg/v1/fill/w_896,h_287,al_c,lg_1,q_80/9f8412_61c81d2acab442dc877e2449ee89ae07~mv2.jpg',
+  'Silent Taste OPT': 'https://static.wixstatic.com/media/9f8412_a31dec9817f3462ba65515d9fec8e40c~mv2.jpg/v1/fill/w_812,h_172,al_c,lg_1,q_80/9f8412_a31dec9817f3462ba65515d9fec8e40c~mv2.jpg',
+  'Crescent Bay': 'https://static.wixstatic.com/media/9f8412_62b7bbdf52f842af90744fd7963e86ae~mv2.jpg/v1/fill/w_768,h_610,al_c,lg_1,q_85/9f8412_62b7bbdf52f842af90744fd7963e86ae~mv2.jpg',
+  'Time Machine WT': 'https://static.wixstatic.com/media/9f8412_0ebcce9846c949979b5683bb0fec2999~mv2.png/v1/fill/w_984,h_759,al_c,q_90/9f8412_0ebcce9846c949979b5683bb0fec2999~mv2.png',
+  'Time Machine OPT': 'https://static.wixstatic.com/media/9f8412_0ebcce9846c949979b5683bb0fec2999~mv2.png/v1/fill/w_984,h_759,al_c,q_90/9f8412_0ebcce9846c949979b5683bb0fec2999~mv2.png',
+  'Ugly Duckling OPT': 'https://static.wixstatic.com/media/9f8412_3ed816a529f245189f143f737d0f9e04~mv2.jpg/v1/fill/w_812,h_218,al_c,lg_1,q_80/9f8412_3ed816a529f245189f143f737d0f9e04~mv2.jpg',
+  'Piglet BK': 'https://static.wixstatic.com/media/9f8412_c10c4e57a5b84129b686a1f3155aec45~mv2.jpg/v1/fill/w_1000,h_621,al_c,q_85,usm_0.66_1.00_0.01/9f8412_c10c4e57a5b84129b686a1f3155aec45~mv2.jpg',
 };
 
 export function getItemImage(itemName) {
